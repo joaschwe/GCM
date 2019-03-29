3716c50136480f2b79da355854d7b3c27b07e05d
joaschwe <joaschwe@iupui.edu>
2019-03-29 18:15:25 -0400

    adding files via upload

---
+++ about_us.php
@@ -0,0 +1,69 @@
+<?php
+require './includes/header.inc.php';
+require './includes/nav.inc.php';
+?>
+
+<div class="container">
+
+    <div class="about">
+
+        <!--about content-->
+        <div class="aboutContent">
+
+            <div class="maintitle">
+                <img src="images/logos/GCM-knot.png" style="display: inline;">
+                <h3 class="clicker-sansSerif">About <span>Good Cause Market</span></h3>
+            </div>
+            <p>
+                Good Cause Market was created out of a need to give charitable organizations a new and unique way to
+                fundraise. The typical fundraiser for schools, churches, clubs and sports is a lot of work and
+                organization. Fundraisers most often run for a short period of time and so the actual funds raised, are
+                limited. Typically, there is an enormous amount of effort involved. Many fundraisers take a lot of
+                planning time and a lot of volunteers. It lasts one day and boom, it's over! All that effort and
+                hopefully it was worth it. But life is pretty compact these days and non-profits need something that
+                will allow them to have revenue all year long with minimal commitment from volunteers and take even less
+                time. GoodCauseMarket.com is an e-store that is open all the time. There is no time-span limit at Good
+                Cause Market. Fundraising for the charitable organization becomes a steady stream of revenue. Plus, the
+                hand-crafted, local, small business products that are offered, replace that unhealthy cookie dough or
+                pizza fundraiser of yester year. Good Cause Market requires only the time it takes to shop for unique,
+                artisan made crafts. 50% of the purchase is sent to the Cause. It's a way for customers to give back
+                without breaking their back! We're glad you came. Now, go do some Fundshopping©!
+            </p>
+
+        </div>
+
+        <!--about image-->
+        <!--        <div class="about-img">-->
+        <!--            <img src="images/logos/GCM-knot.png">-->
+        <!--        </div>-->
+
+    </div>
+</div><!--end container-->
+
+
+<!--FOOTER-->
+<?php
+include './includes/footer.inc.php'
+?>
+
+
+<!--
+h3.title {
+font-family: serif;
+font-size: 42px;
+font-weight: normal;
+text-align: left;
+width:100%;
+margin-bottom:10px;
+color:#6b6b6b;
+}
+h3.title span {
+color: #8DC63F;
+font-style: italic;
+text-decoration: none;
+font-family: "ClickerScript-Regular", serif;
+font-size: 54px;
+text-align: left;
+/*padding-left:10px;*/
+}
+-->
